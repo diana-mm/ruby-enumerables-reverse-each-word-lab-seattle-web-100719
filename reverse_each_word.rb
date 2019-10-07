bacd7e347e15b062d3544c4be16b5ca3ed3166fc
Diana M <dmmiller85@gmail.com>
2019-10-07 05:04:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,3 @@
 
-reverse_each_word("Hello there,", "and how are you?") { |b| "#{b.reverse} on #{"bread".reverse}" }
- 
+def reverse_each_word("Hello there,", "and how are you?") { |b| "#{b.reverse} on #{"bread".reverse}" }
+ end
